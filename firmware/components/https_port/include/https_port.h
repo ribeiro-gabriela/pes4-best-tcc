597587fb4fa2f481263a7cea712dac642fbac419
Pedro Pimenta <pmp@cin.ufpe.br>
2025-07-30 12:06:55 -0300

    feat: HTTPS Server initial setup

---
+++ firmware/components/https_port/include/https_port.h
@@ -0,0 +1,21 @@
+#ifndef _HTTPS_PORT_H
+#define _HTTPS_PORT_H
+
+#include "esp_https_server.h"
+#include "esp_log.h"
+
+void https_event_handler(void* arg, esp_event_base_t event_base, 
+                        int32_t event_id, void* event_data);
+
+httpd_handle_t start_webserver();
+
+// static void connect_handler(void* arg, esp_event_base_t event_base, 
+//                             int32_t event_id, void* event_data);
+
+void init_https_server();
+
+esp_err_t get_hello_handler(httpd_req_t* request);
+
+void https_server_user_callback(esp_https_server_user_cb_arg_t *user_cb);
+
+#endif
