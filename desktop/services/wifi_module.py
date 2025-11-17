2b334f3bab7ed41d4a880815202741c4d3283605
Luciano Mansonari <lcfm@cin.ufpe.br>
2025-11-17 01:04:33 -0300

    remove comments

--- desktop/services/wifi_module.py
+++ desktop/services/wifi_module.py
@@ -8,8 +8,6 @@ import subprocess, tempfile, os
 from data.classes import Connection, Package, Request, Response
 from data.errors import RequestTimeoutError, ConnectionAuthenticationError
 
-import _wmi
-
 class WifiModule:
     _PASSWORD = "bcappassword"
     _tftp_client: TftpClient | None = None
@@ -188,109 +186,6 @@ class WifiModule:
         if not password:
             password = self._PASSWORD
         
-        # with self.lock:
-        #     self.iface.scan()
-        # time.sleep(2)
-        # with self.lock:
-        #     scan_results = self.iface.scan_results()
-
-        # found_profile = next((p for p in scan_results if p.ssid == target), None)
-
-        # profile = found_profile
-
-        # if not found_profile:
-        #     profile = pywifi.Profile()
-        #     profile.ssid = target
-        #     profile.auth = const.AUTH_ALG_OPEN
-        #     profile.akm = []
-        #     profile.cipher = const.CIPHER_TYPE_NONE
-
-        #     def is_psk(p):
-        #         return p and (const.AKM_TYPE_WPA2PSK in p.akm or const.AKM_TYPE_WPAPSK in p.akm)
-        #     def is_open(p):
-        #         return p and (const.AKM_TYPE_NONE in p.akm)
-
-        #     if is_psk(found_profile):
-        #         if not password:
-        #             raise ConnectionAuthenticationError(f"Network '{target}' requires a password (WPA/WPA2-PSK).")
-        #         if const.AKM_TYPE_WPA2PSK in found_profile.akm:
-        #             profile.akm = [const.AKM_TYPE_WPA2PSK]
-        #             profile.cipher = const.CIPHER_TYPE_CCMP
-        #         elif const.AKM_TYPE_WPAPSK in found_profile.akm:
-        #             profile.akm = [const.AKM_TYPE_WPAPSK]
-        #             profile.cipher = const.CIPHER_TYPE_TKIP
-        #         profile.key = password
-
-        #     elif is_open(found_profile) or (not found_profile and not password):
-        #         profile.akm = [const.AKM_TYPE_NONE]
-        #         profile.cipher = const.CIPHER_TYPE_NONE
-        #     else:
-        #         if password:
-        #             profile.akm = [const.AKM_TYPE_WPA2PSK]
-        #             profile.cipher = const.CIPHER_TYPE_CCMP
-        #             profile.key = password
-        #         else:
-        #             raise ConnectionAuthenticationError(
-        #                 f"The security mode for '{target}' could not be determined, and no password was provided."
-        #             )
-
-        # profile.key = password
-
-        # with self.lock:
-        #     self.iface.disconnect()
-        # t0 = time.time()
-        # while time.time() - t0 < 5:
-        #     if self.iface.status() == const.IFACE_DISCONNECTED:
-        #         break
-        #     time.sleep(0.2)
-
-        # with self.lock:
-        #     for p in self.iface.network_profiles():
-        #         if p.ssid == target:
-        #             self.iface.remove_network_profile(p)
-
-        # add_ok = True
-        # try:
-        #     with self.lock:
-        #         tmp_profile = self.iface.add_network_profile(profile)
-        #         if not tmp_profile:
-        #             add_ok = False
-        # except Exception:
-        #     add_ok = False 
-
-        # if add_ok:
-        #     with self.lock:
-        #         self.iface.connect(tmp_profile)
-
-        #     t0 = time.time()
-        #     while time.time() - t0 < 30:
-        #         if self.iface.status() == const.IFACE_CONNECTED:
-        #             ssid_ok = True
-        #             if sys.platform == "win32":
-        #                 ssid_ok = (self._current_ssid_windows() == target)
-        #             if ssid_ok:
-        #                 print(f"Successfully connected to {target}.")
-                        
-        #                 self._tftp_client = TftpClient(target, 69)
-        #                 return Connection(
-        #                     device=target,
-        #                     hardwarePN="HW-PN-REAL-WIFI",
-        #                     address="192.168.1.1",
-        #                     connectedAt=int(time.time()),
-        #                     pauseHealthCheck=False
-        #                 )
-        #         time.sleep(0.5)
-
-        #     with self.lock:
-        #         self.iface.disconnect()
-
-        # if sys.platform == "win32":
-        # iface_name = self.iface.name()
-        # if not iface_name:
-        #     iface_name = "Wi-Fi"
-        # wpa2 = True
-        # if found_profile and const.AKM_TYPE_WPAPSK in found_profile.akm:
-        #     wpa2 = False
         ok = self._win_fallback_connect(target, password)
         if ok:
             print(f"Successfully connected to {target} (via NETSH fallback).")
