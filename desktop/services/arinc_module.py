b29644c782e9060e39720577df558472b9a71ba6
Luciano Mansonari <lcfm@cin.ufpe.br>
2025-12-02 14:57:53 -0300

    update arinc upload progress based on lus counter

--- desktop/services/arinc_module.py
+++ desktop/services/arinc_module.py
@@ -174,8 +174,8 @@ class ArincModule(ITransferProtocol):
                             self.transfer_status.progressPercent = 20
 
                     case LoadProtocolStatusCode.IN_PROGRESS | LoadProtocolStatusCode.IN_PROGRESS_INFO:
-                        if self.transfer_status.progressPercent < 40:
-                            self.transfer_status.progressPercent = 40
+                        if(lus_file.counter):
+                            self.transfer_status.progressPercent = 20 + min(80,int(80*(lus_file.counter*512/self.transfer_status.fileRecord.sizeBytes)))
 
                     case LoadProtocolStatusCode.COMPLETED:
                         self.transfer_status.transferStep = (
